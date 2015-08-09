bfca90642f0eddd89734657f31c2207076a44218
Brian Wolfe <wolfe.br@gmail.com>
2015-08-09 14:34:52 -0400

    Make initial timeseries and subselection types.
    - Initial conversion utilities
    - Initial timeseries datatype
    - 10 unit tests to ensure basic functionality and computation.

--- js/timeseries.js
+++ js/timeseries.js
@@ -14,38 +14,37 @@ var TimeSeries = TimeSeries || {};
  * version of the data.
  */
 TimeSeries.toTypedArray = function(buffer, dataType, littleEndian) {
-  typemap = {
-    // C types
-    'int8_t': {type: Int8Array, conversion: DataView.prototype.getInt8};
-    'uint8_t': {type: Uint8Array, conversion: DataView.prototype.getUint8};
-    'int16_t': {type: Int16Array, conversion: DataView.prototype.getInt16};
-    'uint16_t': {type: Uint16Array, conversion: DataView.prototype.getUint16};
-    'int32_t': {type: Int32Array, conversion: DataView.prototype.getInt32};
-    'uint32_t': {type: Int16Array, conversion: DataView.prototype.getUint32};
-    'float': {type: Float32Array, conversion: DataView.prototype.getFloat32};
-    'double': {type: Float64Array, conversion: DataView.prototype.getFloat64};
-    // Numpy datatypes
-    'int8': {type: Int8Array, conversion: DataView.prototype.getInt8};
-    'uint8': {type: Uint8Array, conversion: DataView.prototype.getUint8};
-    'int16': {type: Int16Array, conversion: DataView.prototype.getInt16};
-    'uint16': {type: Uint16Array, conversion: DataView.prototype.getUint16};
-    'int32': {type: Int32Array, conversion: DataView.prototype.getInt32};
-    'uint32': {type: Uint32Array, conversion: DataView.prototype.getUint32};
-    'float': {type: Float32Array, conversion: DataView.prototype.getFloat32};
-    'double': {type: Float64Array, conversion: DataView.prototype.getFloat64};
-  };
-
+  'use strict';
   if (typeof(dataType) == 'undefined') {
     dataType = 'uint32_t';
   }
 
-  var bigEndian = true;
-
-  if (littleEndian) {
-    bigEndian = false;
+  if (typeof(littleEndian) === 'undefined') {
+    littleEndian = true;
   }
 
-  var dv = DataView(data);
+  var typemap = {
+    // C types
+    'int8_t': {type: Int8Array, conversion: DataView.prototype.getInt8},
+    'uint8_t': {type: Uint8Array, conversion: DataView.prototype.getUint8},
+    'int16_t': {type: Int16Array, conversion: DataView.prototype.getInt16},
+    'uint16_t': {type: Uint16Array, conversion: DataView.prototype.getUint16},
+    'int32_t': {type: Int32Array, conversion: DataView.prototype.getInt32},
+    'uint32_t': {type: Int16Array, conversion: DataView.prototype.getUint32},
+    'float': {type: Float32Array, conversion: DataView.prototype.getFloat32},
+    'double': {type: Float64Array, conversion: DataView.prototype.getFloat64},
+    // Numpy datatypes
+    'int8': {type: Int8Array, conversion: DataView.prototype.getInt8},
+    'uint8': {type: Uint8Array, conversion: DataView.prototype.getUint8},
+    'int16': {type: Int16Array, conversion: DataView.prototype.getInt16},
+    'uint16': {type: Uint16Array, conversion: DataView.prototype.getUint16},
+    'int32': {type: Int32Array, conversion: DataView.prototype.getInt32},
+    'uint32': {type: Uint32Array, conversion: DataView.prototype.getUint32},
+    'float32': {type: Float32Array, conversion: DataView.prototype.getFloat32},
+    'float64': {type: Float64Array, conversion: DataView.prototype.getFloat64}
+  };
+
+  var dv = new DataView(buffer);
 
   var offset;
   var curtype = typemap[dataType].type;
@@ -55,7 +54,7 @@ TimeSeries.toTypedArray = function(buffer, dataType, littleEndian) {
   var typedArray = new curtype(n_elements);
 
   for (offset = 0; offset < n_elements; offset ++) {
-    typedArray[offset] = conversion.prototype.call(dv, offset * inc)
+    typedArray[offset] = conversion.call(dv, offset * inc, littleEndian)
   }
 
   return typedArray;
@@ -63,14 +62,177 @@ TimeSeries.toTypedArray = function(buffer, dataType, littleEndian) {
 
 /**
  * Given a raw time buffer, data buffer, and a type for the data,
- * construct a timeseries object that contains the 
+ * construct a timeseries object that can be used to extract meaningful
+ * data at particular points in time and over particular windows in time.
+ *
+ * \param time  A typed array containing the time values to index the data with
+ * \param data  A typed array containing the data values associated with each timepoint
+ * 
+ * Restriction: data.length % time.length == 0. The width of the data
+ *              (number of samples per timestamp should be constant, ensuring this).
+ *              If data.length % time.length != 0 the series alignment may be
+ *              corrupted.
  */
-TimeSeries.SubsampleSeries = function(time, data, dtype) {
-  var time_dv = new TimeSeries.DataArray(time, 'uint32');
-  var data_dv = new DataView(data);
+TimeSeries.BasicSeries = function(time, data, dtype) {
+  'use strict';
+  this.time = time;
+  this.data = data;
+  this.width = Math.floor(data.length / time.length);
 
-  if (typeof(dtype) == 'undefined') {
+  if (data.length % time.length != 0) {
+    console.log("Invalid data provided, the data length must equal the time length");
   }
 }
 
+/**
+ * given a target element el and a sorted array arr, search for el in arr,
+ * returning the index of el if it exists. If el is less than any element in
+ * arr, return 0, otherwise, return the index of the element in arr that
+ * would precede el if el were inserted.
+ */
+TimeSeries.binarySearch = function(el, arr) {
+  'use strict';
+  var min_index = 0;
+  var max_index = arr.length;
+  var diff;
+  var cur_index;
+  var cur_el;
 
+  while (min_index <= max_index) {
+    cur_index = Math.floor((max_index - min_index) / 2) + min_index;
+    cur_el = arr[cur_index];
+
+    if (cur_el < el) {
+      min_index = cur_index + 1;
+    } else if (cur_el > el) {
+      max_index = cur_index - 1;
+    } else {
+      return cur_index;
+    }
+  }
+
+  return max_index;
+}
+
+/**
+ * Return the data (this.width samples) for the last sample with timestamp
+ * at most t.
+ *
+ * This returns a typed array with the same type as the raw data.
+ */
+TimeSeries.BasicSeries.prototype.asof = function(t) {
+  'use strict';
+  var ind = TimeSeries.binarySearch(t, this.time);
+  var s_ind = ind * this.width;
+  return this.data.subarray(s_ind, s_ind + this.width);
+}
+
+/**
+ * \param  lowtime     lower time to include, one sample beyond this time can be included
+ * \param  hightime    higher time to include, one sample beyond this time can be included
+ * \param  maxpoints   maximum number of points (default 1000)
+ * return: a subsampled window of timestamps as a normal array with each
+ *  element in the array being an associative array with the following
+ *  entries:
+ *  {
+ *    time: average time of sample
+ *    mean: [array of means, one for each subsample]
+ *    max: [array of maximums, one for each subsample]
+ *    min: [array of minimums, one for each subsample]
+ *    std: [array of standard deviations, one for each subsample]
+ *    num: number of samples
+ *  }
+ *
+ */
+TimeSeries.BasicSeries.prototype.getValues = function(lowtime, hightime, maxpoints) {
+  'use strict';
+
+  if (typeof(lowtime) == 'undefined') {
+    lowtime = this.time[0];
+  }
+  if (typeof(hightime) == 'undefined') {
+    hightime = this.time[this.time.length - 1];
+  }
+  if (typeof(maxpoints) == 'undefined') {
+    maxpoints = 1000;
+  }
+  var start_ind = TimeSeries.binarySearch(lowtime, this.time);
+  var end_ind = TimeSeries.binarySearch(hightime, this.time);
+
+  if (end_ind < this.time.length - 1) {
+    end_ind ++;
+  }
+
+  var start_time = this.time[start_ind];
+  var end_time = this.time[end_ind];
+
+  var interval = (end_time - start_time) / maxpoints;
+
+  var cur_time = start_time,
+      mean_time = 0,
+      mean = new Float64Array(this.width),
+      squared_mean = new Float64Array(this.width),
+      max = this.data.subarray(start_ind * this.width, (start_ind + 1) * this.width),
+      min = this.data.subarray(start_ind * this.width, (start_ind + 1) * this.width),
+      num = 0,
+      res = [];
+
+  for (var i = start_ind; i <= end_ind; i++) {
+    if (cur_time + interval < this.time[i]) {
+      var std = new Float64Array(this.width);
+      for (var j = 0; j < this.width; j++) {
+        mean[j] /= num;
+        squared_mean[j] /= num;
+        std[j] = Math.sqrt(squared_mean[j] - mean[j] * mean[j]);
+      }
+      res.push({
+        time: mean_time / num,
+        mean: mean,
+        std: std,
+        min: min,
+        max: max,
+        num: num
+      });
+      mean_time = 0;
+      mean = new Float64Array(this.width);
+      squared_mean = new Float64Array(this.width);
+      max = this.data.subarray(i * this.width, (i + 1) * this.width);
+      min = this.data.subarray(i * this.width, (i + 1) * this.width);
+      num = 0;
+      // Mark the start of the next interval. Taking the max of these two moves
+      // the sample point forward to the next timepoint if the interval is less than
+      // the typical timepoint.
+      cur_time = Math.max(cur_time + interval, this.time[i + 1] - interval * 0.99);
+    }
+
+    mean_time += this.time[i];
+    num ++;
+    for (var j = 0; j < this.width; j++) {
+      var cur_sample = this.data[i * this.width + j];
+      mean[j] += cur_sample;
+      squared_mean[j] += cur_sample * cur_sample;
+      max[j] = Math.max(max[j], cur_sample);
+      min[j] = Math.min(min[j], cur_sample);
+    }
+  }
+
+  // Append the final sample if necessary.
+  if (num > 0) {
+    std = new Float64Array(this.width);
+    for (var j = 0; j < this.width; j++) {
+      mean[j] /= num;
+      squared_mean[j] /= num;
+      std[j] = Math.sqrt(squared_mean[j] - mean[j] * mean[j]);
+    }
+    res.push({
+      time: mean_time / num,
+      mean: mean,
+      std: std,
+      min: min,
+      max: max,
+      num: num
+    });
+  }
+
+  return res;
+}
--- tests/test-timeseries.js
+++ tests/test-timeseries.js
@@ -1,6 +1,128 @@
+function make_random_array_uint32(len) {
+  var i;
+  var a = new Uint32Array(len);
+  for (i = 0; i < len; i++) {
+    a[i] = Math.random() * 32767 * 32767;
+  }
+  return a;
+}
+
+function make_random_array_int32(len) {
+  var i;
+  var a = new Int32Array(len);
+  for (i = 0; i < len; i++) {
+    a[i] = Math.random() * 32767 * 32767;
+  }
+  return a;
+}
+
+function make_random_array_float32(len) {
+  var i;
+  var a = new Float32Array(len);
+  for (i = 0; i < len; i++) {
+    a[i] = (Math.random() - 0.5) * 1000;
+  }
+  return a;
+}
+
+function check_equal(a1, a2) {
+  expect(a1.length).toBe(a2.length);
+
+  var total_diff = 0;
+  for (var i = 0; i < a1.length; i ++) {
+    total_diff += Math.abs(a1[i] - a2[i]);
+  }
+
+  expect(total_diff).toBe(0);
+}
 
 describe("Typed array conversion tests", function () {
   it("Convert uint32 typed array", function() {
-    expect(true).toBe(false);
+    var len = 500;
+    test_data = make_random_array_int32(len);
+    data = TimeSeries.toTypedArray(test_data.buffer, 'int32_t', true);
+    check_equal(data, test_data);
+  });
+
+  it("Convert float32 typed array", function() {
+    var len = 5;
+    test_data = make_random_array_float32(len);
+    data = TimeSeries.toTypedArray(test_data.buffer, 'float32', true);
+    check_equal(data, test_data);
+  });
+
+  it("Convert to twice to reverse byte orders", function () {
+    var len = 100;
+    test_data = make_random_array_uint32(len);
+
+    data_tmp = TimeSeries.toTypedArray(test_data.buffer, 'uint32', false);
+    data_bigendian = TimeSeries.toTypedArray(data_tmp.buffer, 'uint32', false);
+
+    data_tmp = TimeSeries.toTypedArray(test_data.buffer, 'uint32', true);
+    data_littleendian = TimeSeries.toTypedArray(data_tmp.buffer, 'uint32', true);
+
+    check_equal(data_bigendian, test_data);
+    check_equal(data_littleendian, test_data);
+  });
+});
+
+function make_time_data(interval, n_samples) {
+  var i;
+  var a = new Uint32Array(n_samples);
+  for (i = 0; i < n_samples; i++) {
+    a[i] = i * interval;
+  }
+  return a;
+}
+
+describe("Test SubsampleSeries", function() {
+  it("Check time alignment of series", function() {
+    // Construct a basic time series with 1 column with 3 subcolumns, sampled
+    // every 20 ms.
+    var time_data = make_time_data(20, 100);
+    var values = make_random_array_uint32(300);
+    var series = new TimeSeries.BasicSeries(time_data, values, 'uint32');
+
+    var sample = series.asof(80);
+    var offset = 4;
+
+    for (var i = 0; i < 3; i++)
+    {
+      expect(sample[i]).toBe(values[3 * offset + i]);
+    }
+  });
+
+  it("Check basic sanity of getting a data window", function() {
+    var time_data = make_time_data(10, 1000);
+    var values = make_random_array_uint32(3000);
+    var series = new TimeSeries.BasicSeries(time_data, values, 'uint32');
+
+    var samples = series.getValues(0, 1000, 10);
+
+    expect(samples[0].num, 5);
+    expect(samples[1].num, 5);
+    expect(samples[0].time, 40);
+    expect(samples[0].mean[0],
+        (values[0] + values[3] + values[6] + values[9] + values[12]) / 5);
+
+    var variance = 0;
+    var max = values[0];
+    var min = values[0];
+    for (var i = 0; i < 5; i++) {
+      variance += Math.pow(values[i * 3] - samples[0].mean[0], 2);
+      max = Math.max(max, values[i * 3]);
+      min = Math.min(min, values[i * 3]);
+    }
+
+    variance /= 5;
+
+    var std = Math.sqrt(variance);
+
+    expect(samples[0].std[0], std);
+
+    expect(samples[0].max, max);
+    expect(samples[0].min, min);
+
+    console.log(samples);
   });
 });
